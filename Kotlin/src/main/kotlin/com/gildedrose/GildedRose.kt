a013b75de0299d6e1ea33087b7f9167572e79839
Lewie Mortier <lewiemortier@gmail.com>
2021-06-22 21:31:01 +0200

    Add unit tests for existing cases

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,11 +2,17 @@ package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
 
+    companion object {
+        private const val AGED_BRIE = "Aged Brie"
+        private const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+        private const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+    }
+
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (items[i].name != AGED_BRIE && items[i].name != BACKSTAGE_PASSES) {
                 if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                    if (items[i].name != SULFURAS) {
                         items[i].quality = items[i].quality - 1
                     }
                 }
@@ -14,7 +20,7 @@ class GildedRose(var items: Array<Item>) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (items[i].name == BACKSTAGE_PASSES) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1
@@ -30,15 +36,15 @@ class GildedRose(var items: Array<Item>) {
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (items[i].name != SULFURAS) {
                 items[i].sellIn = items[i].sellIn - 1
             }
 
             if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (items[i].name != AGED_BRIE) {
+                    if (items[i].name != BACKSTAGE_PASSES) {
                         if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                            if (items[i].name != SULFURAS) {
                                 items[i].quality = items[i].quality - 1
                             }
                         }
@@ -53,6 +59,5 @@ class GildedRose(var items: Array<Item>) {
             }
         }
     }
-
 }
 
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -48,7 +48,7 @@ internal class GildedRoseTest {
     }
 
     @Test
-    internal fun `Quality decreases after sellin reached 0 and quality is 0`() {
+    internal fun `Quality decreases after sellIn reached 0 and quality is 0`() {
         val item = Item("foo", 0, 2)
         val app = GildedRose(arrayOf(item))
         app.updateQuality()
@@ -56,6 +56,157 @@ internal class GildedRoseTest {
         app.assertQualityAndSellIn(
                 expectedSellIn = -1,
                 expectedQuality = 0
+        )
+    }
+
+    @Test
+    internal fun `Quality can never be negative`() {
+        val item = Item("foo", 3, 1)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = 2,
+                expectedQuality = 0
+        )
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = 1,
+                expectedQuality = 0
+        )
+    }
+
+    @Test
+    internal fun `Aged Brie increases in quality`() {
+        val item = Item("Aged Brie", 3, 3)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = 2,
+                expectedQuality = 4
+        )
+    }
+
+    @Test
+    internal fun `Aged Brie increases quality after sellIn date`() {
+        val item = Item("Aged Brie", 0, 3)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = -1,
+                expectedQuality = 5
+        )
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = -2,
+                expectedQuality = 7
+        )
+    }
+
+    @Test
+    internal fun `Aged Brie is case sensitive`() {
+        val item = Item("aged brie", 3, 3)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = 2,
+                expectedQuality = 2
+        )
+    }
+
+    @Test
+    internal fun `Quality can never go over 50`() {
+        val item = Item("Aged Brie", 3, 49)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = 2,
+                expectedQuality = 50
+        )
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = 1,
+                expectedQuality = 50
+        )
+    }
+
+    @Test
+    internal fun `Sulfuras never decreases in quality`() {
+        val item = Item("Sulfuras, Hand of Ragnaros", 20, 20)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = 20,
+                expectedQuality = 20
+        )
+    }
+
+    @Test
+    internal fun `Backstage passes increase in quality`() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 20, 20)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = 19,
+                expectedQuality = 21
+        )
+    }
+
+    @Test
+    internal fun `Backstage passes increase in quality 11 days before sellIn`() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = 9,
+                expectedQuality = 22
+        )
+    }
+
+    @Test
+    internal fun `Backstage passes increase in quality 5 days before sellIn`() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 4, 20)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = 3,
+                expectedQuality = 23
+        )
+    }
+
+    @Test
+    internal fun `Backstage passes lose quality after sellIn date`() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = -1,
+                expectedQuality = 0
+        )
+    }
+
+    @Test
+    internal fun `Backstage passes cannot go over 50 in quality`() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 1, 48)
+
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+        app.assertQualityAndSellIn(
+                expectedSellIn = 0,
+                expectedQuality = 50
         )
     }
 
